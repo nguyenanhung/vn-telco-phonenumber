ee9967d233aa0290031d1628e0d923d430644e50
nguyenanhung <info@nguyenanhung.com>
2023-02-22 22:18:45 +0700

    Release next-gen version v2.x

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -18,36 +18,36 @@ namespace nguyenanhung\VnTelcoPhoneNumber;
  */
 interface ProjectInterface
 {
-    const VERSION                  = '2.1.0';
-    const LAST_MODIFIED            = '2021-09-25';
-    const DEFAULT_COUNTRY          = 'VN';
-    const DEFAULT_LANGUAGE         = 'vi';
-    const DEFAULT_REGION           = 'VN';
-    const DEFAULT_REGION_CODE      = '84';
-    const HIDDEN_STRING            = '*';
+    const VERSION = '2.1.1';
+    const LAST_MODIFIED = '2023-02-22';
+    const DEFAULT_COUNTRY = 'VN';
+    const DEFAULT_LANGUAGE = 'vi';
+    const DEFAULT_REGION = 'VN';
+    const DEFAULT_REGION_CODE = '84';
+    const HIDDEN_STRING = '*';
     const FORMAT_FOR_HUMAN_VIETNAM = 'VN_HUMAN';
-    const FORMAT_E164              = 'E164';
-    const FORMAT_INTERNATIONAL     = 'INTERNATIONAL'; // International
-    const FORMAT_NATIONAL          = 'NATIONAL'; // National
-    const FORMAT_RFC3966           = 'RFC3966';
-    const HIDDEN_REGION            = 'HIDDEN';
-    const HIDDEN_REGION_HEAD       = 'HIDDEN_HEAD';
-    const HIDDEN_REGION_MIDDLE     = 'HIDDEN_MIDDLE';
-    const HIDDEN_REGION_END        = 'HIDDEN_END';
-    const HIDDEN_PLACE_HEAD        = 'HIDDEN_HEAD'; // Ẩn số đầu
-    const HIDDEN_PLACE_MIDDLE      = 'HIDDEN_MIDDLE'; // Ẩn số giữa
-    const HIDDEN_PLACE_END         = 'HIDDEN_END'; // Ẩn số cuối
-    const CONVERT_NEW_TO_OLD       = 'old';
-    const CONVERT_OLD_TO_NEW       = 'new';
-    const MATCH_NUMBER_OLD         = '/^(841[2689])[0-9]{8}$/';
-    const MATCH_NUMBER_NEW         = '/^(84[3785])[0-9]{8}$/';
-    const MAX_LENGTH_NUMBER_OLD    = 12;
-    const MAX_LENGTH_NUMBER_NEW    = 11;
-    const VINAPHONE                = 'Vinaphone'; // ID nhà mạng Vinaphone
-    const VIETTEL                  = 'Viettel Mobile'; // ID nhà mạng Viettel
-    const MOBIFONE                 = 'MobiFone'; // ID nhà mạng MobiFone
-    const VIETNAMOBILE             = 'Vietnamobile'; // ID nhà mạng Vietnamobile
-    const USE_BENCHMARK            = false;
+    const FORMAT_E164 = 'E164';
+    const FORMAT_INTERNATIONAL = 'INTERNATIONAL'; // International
+    const FORMAT_NATIONAL = 'NATIONAL'; // National
+    const FORMAT_RFC3966 = 'RFC3966';
+    const HIDDEN_REGION = 'HIDDEN';
+    const HIDDEN_REGION_HEAD = 'HIDDEN_HEAD';
+    const HIDDEN_REGION_MIDDLE = 'HIDDEN_MIDDLE';
+    const HIDDEN_REGION_END = 'HIDDEN_END';
+    const HIDDEN_PLACE_HEAD = 'HIDDEN_HEAD'; // Ẩn số đầu
+    const HIDDEN_PLACE_MIDDLE = 'HIDDEN_MIDDLE'; // Ẩn số giữa
+    const HIDDEN_PLACE_END = 'HIDDEN_END'; // Ẩn số cuối
+    const CONVERT_NEW_TO_OLD = 'old';
+    const CONVERT_OLD_TO_NEW = 'new';
+    const MATCH_NUMBER_OLD = '/^(841[2689])[0-9]{8}$/';
+    const MATCH_NUMBER_NEW = '/^(84[3785])[0-9]{8}$/';
+    const MAX_LENGTH_NUMBER_OLD = 12;
+    const MAX_LENGTH_NUMBER_NEW = 11;
+    const VINAPHONE = 'Vinaphone'; // ID nhà mạng Vinaphone
+    const VIETTEL = 'Viettel Mobile'; // ID nhà mạng Viettel
+    const MOBIFONE = 'MobiFone'; // ID nhà mạng MobiFone
+    const VIETNAMOBILE = 'Vietnamobile'; // ID nhà mạng Vietnamobile
+    const USE_BENCHMARK = false;
 
     /**
      * Get current version of Package
